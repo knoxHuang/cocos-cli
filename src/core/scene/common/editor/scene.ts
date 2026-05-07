1df32d61eee04a07feef3d4d00f02cfa56e5b1f3
hyde zhou <zlzhou.sh@outlook.com>
2026-05-07 18:05:44 +0800

    Reapply "Enhance dump-related interfaces and add node-related interfaces (#521)" (#533) (#538)
    This reverts commit d5c2b836e04ae98d09803cf09a30538066de80b2.

--- src/core/scene/common/editor/scene.ts
+++ src/core/scene/common/editor/scene.ts
@@ -2,6 +2,7 @@ import type { INode } from '../node';
 import type { IComponentIdentifier } from '../component';
 import type { IBaseIdentifier } from './base';
 import { IPrefabInfo } from '../prefab';
+import { IProperty } from '../../@types/public';
 
 /**
  * 场景信息
@@ -11,4 +12,19 @@ export interface IScene extends IBaseIdentifier {
     prefab: IPrefabInfo | null,
     children: INode[];
     components: IComponentIdentifier[];
+}
+
+export interface ISceneForEditor {
+    name: IProperty;
+    active: IProperty;
+    locked: IProperty;
+    _globals: any;
+    isScene: boolean;
+    autoReleaseAssets: IProperty;
+
+    uuid: IProperty;
+    children: any[];
+    parent: any;
+    __type__: string;
+    targetOverrides?: any;
 }
